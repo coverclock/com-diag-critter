15fa2553ef0f6dbe0dde7be0eff831a884e98a10
Chip Overclock <coverclock@diag.com>
2019-01-21 09:44:20 -0700

    [1] Softlink projects to src and changed setup. [2] TESTED.

--- Critter/bin/setup.sh
+++ Critter/bin/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/basE
 # ${HOME}/src/com-diag-hazer
 # ${HOME}/src/com-diag-critter
-# ${HOME}/Projects/critter
+# ${HOME}/src/gnuradio
+# ${HOME}/src/gnss-sdr
 SELF=${SUDO_USER:-${USER}}
 MINE=$(eval cd ~${SELF};pwd)
+ROOT=$(readlink -f ${MINE}/src)
 HAZER=$(readlink -f ${MINE}/src/com-diag-hazer)
 CRITTER=$(readlink -f ${MINE}/src/com-diag-critter)
-WORK=$(readlink -f ${MINE}/Projects/critter)
-GNURADIO="${WORK}/gnuradio"
-GNSSSDR="${WORK}/gnss-sdr"
+GNURADIO=$(readlink -f ${MINE}/src/gnuradio)
+GNSSSDR=$(readlink -f ${MINE}/src/gnss-sdr)
 . ${HAZER}/Hazer/out/host/bin/setup
 . ${GNURADIO}/setup_env.sh
 export PATH="${GNSSSDR}/usr/local/bin:$PATH"
