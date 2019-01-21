bcdf808347f1d177ae238ec38926dbe5845f2e27
Chip Overclock <coverclock@diag.com>
2019-01-21 08:26:31 -0700

    [1] Added Hazer setup to Critter setup. [2] Updated README.

--- Critter/bin/setup.sh
+++ Critter/bin/setup.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/basE
+# ${HOME}/src/com-diag-hazer
+# ${HOME}/src/com-diag-critter
+# ${HOME}/Projects/critter
 SELF=${SUDO_USER:-${USER}}
-HERE=$(eval cd ~${SELF}; pwd)
-ROOT="${HERE}/Projects/critter"
-GNURADIO="${ROOT}/gnuradio"
-GNSSSDR="${ROOT}/gnss-sdr"
+MINE=$(eval cd ~${SELF};pwd)
+HAZER=$(readlink -f ${MINE}/src/com-diag-hazer)
+CRITTER=$(readlink -f ${MINE}/src/com-diag-critter)
+WORK=$(readlink -f ${MINE}/Projects/critter)
+GNURADIO="${WORK}/gnuradio"
+GNSSSDR="${WORK}/gnss-sdr"
+. ${HAZER}/Hazer/out/host/bin/setup
 . ${GNURADIO}/setup_env.sh
 export PATH="${GNSSSDR}/usr/local/bin:$PATH"
