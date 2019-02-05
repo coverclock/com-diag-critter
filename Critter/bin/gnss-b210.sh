96c7cc36b5c12afdd29dd6efc12dfe2d125395dd
Chip Overclock <coverclock@diag.com>
2019-02-05 09:55:24 -0700

    [1] Checkpointing.

--- Critter/bin/gnss-b210.sh
+++ Critter/bin/gnss-b210.sh
@@ -3,9 +3,9 @@ HERE=$(dirname ${BASH_SOURCE})
 THERE=$(cd ${HERE}; pwd)
 ROOT=$(readlink -f ${THERE}/../../..)
 CRITTER=$(readlink -f ${ROOT}/com-diag-critter)
-WORK=${CRITTER}/Critter/tmp/gnss-b210
+CONF=gnss-b210
+WORK=${CRITTER}/Critter/tmp/${CONF}
 mkdir -p ${WORK}
 cd ${WORK}
 . ../../bin/setup.sh
-CONF=gnss-b210
 gnss-sdr --config_file=../../etc/${CONF}.conf --log_dir=. -logtostderr -colorlogtostderr
