5613cc00b7772ddd241f06ac6e6b6477aac038fd
Chip Overclock <coverclock@diag.com>
2019-02-13 09:35:26 -0700

    [1] Added legacy conf and script to use old L1 PVT instead of RTKLIB. [2] Testing.

---
+++ Critter/bin/gnss-b210-legacy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2019 Digital Aggregates Corporation, Arvada Colorado USA
+# Licensed under the terms in LICENSE.txt
+CONF=gnss-b210-legacy
+HERE=$(dirname ${BASH_SOURCE})
+THERE=$(cd ${HERE}; pwd)
+ROOT=$(readlink -f ${THERE}/../../..)
+CRITTER=$(readlink -f ${ROOT}/com-diag-critter)
+WORK=${CRITTER}/Critter/tmp/${CONF}
+mkdir -p ${WORK}
+cd ${WORK}
+. ../../bin/setup.sh
+gnss-sdr --config_file=../../etc/${CONF}.conf --log_dir=. -logtostderr -colorlogtostderr
