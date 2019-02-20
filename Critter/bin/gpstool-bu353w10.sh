41a5c95fe2b14c45f0629db743dd6a8013a55bb1
Chip Overclock <coverclock@diag.com>
2019-02-20 10:41:23 -0700

    [1] Reduced gnss-sdr channels to 4 from 8 to try to improve latency (TESTING). [2] Added PATH setup to bu353w10 script (TESTED). [3] Adding experimental test conf.

--- Critter/bin/gpstool-bu353w10.sh
+++ Critter/bin/gpstool-bu353w10.sh
@@ -4,8 +4,15 @@
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-critter
 
-PROGRAM=$(basename ${0})
+PROGRAM=$(basename $0 ".sh")
 DEVICE=${1:-"/dev/ttyACM0"}
 RATE=${2:-9600}
-
-gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 2> /dev/null
+HERE=$(dirname ${BASH_SOURCE})
+THERE=$(cd ${HERE}; pwd)
+ROOT=$(readlink -f ${THERE}/../../..)
+CRITTER=$(readlink -f ${ROOT}/com-diag-critter)
+WORK=${CRITTER}/Critter/tmp/${PROGRAM}
+mkdir -p ${WORK}
+cd ${WORK}
+. ../../bin/setup.sh
+gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 2> ${PROGRAM}.err
